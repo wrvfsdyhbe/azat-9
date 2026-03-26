c91efbcd831f68285348cb694bbc0c3f4663edd9
wrvfsdyhbe <rahimov6762@gmail.com>
2026-03-26 10:46:31 +0300

    Fixed submodules and moved workflows

---
+++ lab8/main.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(
+    filename = 'app.log',
+    level=loggin.INFO,
+    format='%(asctime)s -%(levelname)s -%(message)s',
+    encoding='utf-8'
+)
+print("fdfdf")
