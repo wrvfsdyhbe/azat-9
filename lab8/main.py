504bb013639e1fbbf829367f641e001b4406ef9f
wrvfsdyhbe <rahimov6762@gmail.com>
2026-04-02 10:23:17 +0300

    fix fix2

--- lab8/main.py
+++ lab8/main.py
@@ -1,9 +1,11 @@
 import logging
 
 logging.basicConfig(
-    filename = 'app.log',
-    level=loggin.INFO,
-    format='%(asctime)s -%(levelname)s -%(message)s',
+    filename='app.log',
+    level=logging.INFO,  # исправлено loggin → logging
+    format='%(asctime)s - %(levelname)s - %(message)s',
     encoding='utf-8'
 )
+
 print("fdfdf")
+logging.info("Program run successfully")  # чтобы что-то реально в лог писалось
